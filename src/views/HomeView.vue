a03708465b2b6f23538fdacad4d0b8c56fa39ac4
Suchea <bsuchea@gmail.com>
2024-01-06 16:37:52 +0700

    test list and condition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,42 @@
 
 <template>
-  <div v-for="(ret, index) in books" :key="ret.Id" class="book">
-    <div>{{ ret.Name }} ({{ ret.Description }})</div>
+  <div style="font-size: 25px;">
+    <input v-model="student" type="text" style="font-size: 25px;">
+    <button @click="add">Add</button>
+  </div>
+  <div v-if="check">
+    <h2>Book List</h2>
+    <ul>
+      <li v-for="ret in books">{{ ret }}</li>
+    </ul>
+  </div>
+  <div v-else-if="!check&books.length>3">
+     Read More
+  </div>
+  <div v-else>
+      <h2>Computer List</h2>
+      <ul>
+        <li v-for="ret in computers">{{ ret }}</li>
+      </ul>
   </div>
 </template>
 <script>
 export default{
   data(){
     return {
+      check: 0,
+      student: '',
+      studentList: [],
       books: [
-        {Id: 1, Name: "Javascript" , Description: "Learn More do more with Javascript"},
-        {Id: 2, Name: "Java Programming" , Description: "basic Java Programming start from scratch"},
-        {Id: 3, Name: "C# Programming" , Description: ""},
+        'javascript', 'vue js', 'c# Programming', 'Database'
       ],
-      assigments: []
+      computers: ['Apple Mac Book', 'Asus', 'Lenovo', 'Dell']
+    }
+  },
+  methods: {
+    add(){
+      console.log(this.student);
     }
   }
 }
 </script>
-<style>
-.book{
-  font-size:18px;
-}
-</style>
